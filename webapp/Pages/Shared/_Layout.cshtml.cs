3986c7a5f3eab3ae6536b512c3542eaf5a05a3c1
ihavenolife <matuswall@gmail.com>
2024-05-22 14:40:52 +0200

    basic asp.net webapp running

---
+++ webapp/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,13 @@
+namespace webapp.Pages;
+
+public class _Layout
+{
+    public static bool IsLoggedIn()
+    {
+        return true;
+    }
+    public static string Username()
+    {
+        return "PH USERNAME";
+    }
+}
