2763323b07f7ac96455e327dd1fa31ccb78909f8
ihavenolife <matuswall@gmail.com>
2025-09-11 18:40:01 +0200

    stupid hack

--- https-setup/certbot_entrypoint.sh
+++ https-setup/certbot_entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+echo "Starting certbot... Waiting 30 seconds for nginx to start"
+sleep 30
 certbot certonly --webroot -w /var/www/html --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d $ORIGIN_URL
