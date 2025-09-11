98ecf4d40db59d5a8f7c973600fecc5e261568f8
ihavenolife <matuswall@gmail.com>
2025-09-11 21:35:05 +0200

    loading indicators on shit that takes long

---
+++ vue-mpa/src/components/LoadingIndicator.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="spinner"></div>
+</template>
+
+<style>
+.spinner {
+    border: 8px solid #f3f3f3;
+    /* Light grey */
+    border-top: 8px solid var(--accent);
+    /* Blue */
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    animation: spin 1s linear infinite;
+    margin: 50px auto;
+    /* Center horizontally */
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
+
+<script>
+export default {
+    name: "LoadingIndicator"
+}
+</script>
