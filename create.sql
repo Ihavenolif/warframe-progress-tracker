aa46b919e374a6ca5bc76dfac28f0980fc8a9e73
ihavenolife <matuswall@gmail.com>
2025-09-13 20:46:56 +0200

    post-0.2.1 merge of sql

--- create.sql
+++ create.sql
@@ -119,6 +119,26 @@ CREATE TABLE recipe_ingredients (
     PRIMARY KEY (recipe_name, item_ingredient)
 );
 
+CREATE TABLE missions (
+    unique_name VARCHAR(256) PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    planet VARCHAR(256) NOT NULL,
+    mastery_xp INT NOT NULL,
+    type VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE player_mission_completion (
+    unique_name VARCHAR(256) REFERENCES missions(unique_name),
+    player_id INT REFERENCES player(id),
+    PRIMARY KEY (unique_name, player_id),
+    completes INT DEFAULT 0 NOT NULL,
+    sp_complete BOOLEAN DEFAULT FALSE NOT NULL
+);
+
+ALTER TABLE player ADD COLUMN IF NOT EXISTS duviri_skills INT NOT NULL DEFAULT 0;
+ALTER TABLE player ADD COLUMN IF NOT EXISTS railjack_skills INT NOT NULL DEFAULT 0;
+ALTER TABLE player ADD COLUMN IF NOT EXISTS total_mastery_xp INT NOT NULL DEFAULT 0;
+
 CREATE OR REPLACE FUNCTION ADD_NAME_TO_BLUEPRINT_TRIGGER()
 RETURNS TRIGGER AS $$
 BEGIN
